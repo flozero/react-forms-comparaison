2a2971aaeae7c80d43bd58d41190030b65f3a404
florent giraud <hello@florent.dev>
2023-06-13 22:46:32 -0400

    chore: stop using components from formik to only functions from the api

--- formik/src/App.tsx
+++ formik/src/App.tsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useFormik } from "formik";
 import { z } from "zod";
 
 const schema = z
@@ -8,41 +8,56 @@ const schema = z
   })
   .required();
 
-const Basic = () => (
-  <div>
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      validate={(values) => {
-        const errors = {};
+const Basic = () => {
+  const validate = (values) => {
+    const errors = {};
 
-        const { success, error } = schema.safeParse(values);
+    const { success, error } = schema.safeParse(values);
 
-        if (!success) {
-          error.issues.forEach((e) => {
-            errors[e.path[0]] = e.message;
-          });
-        }
+    if (!success) {
+      error.issues.forEach((e) => {
+        errors[e.path[0]] = e.message;
+      });
+    }
 
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="p" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="p" />
-          <button disabled={isSubmitting}>Submit</button>
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+    validate,
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      {formik.errors.email ? <p role="alert">{formik.errors.email}</p> : null}
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
+      {formik.errors.password ? (
+        <p role="alert">{formik.errors.password}</p>
+      ) : null}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Basic;
