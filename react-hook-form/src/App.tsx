b24dd295d0a4aaf2a923bf9ac3aef7cf75ca199c
florent giraud <hello@florent.dev>
2023-06-13 21:19:23 -0400

    feat: add inputs email and password

--- react-hook-form/src/App.tsx
+++ react-hook-form/src/App.tsx
@@ -1,33 +1,21 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  example: string;
-  exampleRequired: string;
+  email: string;
+  password: string;
 };
 
 export default function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
+  const { register, handleSubmit } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
-
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <input type="submit" />
+      <label>Email</label>
+      <input {...register("email")} />
+      <label>Password</label>
+      <input {...register("password", { required: true })} />
+      <button>Submit</button>
     </form>
   );
 }
